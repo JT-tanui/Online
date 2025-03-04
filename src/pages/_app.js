0163d07e635e5891d9da79be38602c119eb13b41
Job Busienei <106816642+JT-tanui@users.noreply.github.com>
2025-03-05 02:15:34 +0300

    Deployment Commit - Completion of creation of version 1.8

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../styles/globals.css';
 import { ThemeProvider } from '../context/ThemeContext';
 
 function MyApp({ Component, pageProps }) {
-  // Fix for server-side rendering of theme
-  useEffect(() => {
-    // On initial load, check if a theme is stored
-    const savedTheme = localStorage.getItem('theme');
-    
-    if (savedTheme) {
-      // Apply the saved theme
-      document.documentElement.setAttribute('data-theme', savedTheme);
-    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      // Apply dark theme if user prefers dark mode
-      document.documentElement.setAttribute('data-theme', 'dark');
-      localStorage.setItem('theme', 'dark');
-    }
-  }, []);
-
   return (
     <ThemeProvider>
       <Component {...pageProps} />
